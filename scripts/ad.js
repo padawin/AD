1ddbd2528b5abeaf0871073d6cbbbbf4e4b89911
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2014-05-22 17:09:42 +0100

    canvas context created

--- scripts/ad.js
+++ scripts/ad.js
@@ -1,5 +1,16 @@
 (function(){
 	var ad = function(parent){
+		if (parent.tagName != 'CANVAS') {
+			throw 'The parent element must be a canvas tag';
+		}
+
+		this.parent = parent;
+
+		if (!this.parent.getContext) {
+			throw 'This browser does not support the use of canvas';
+		}
+
+		this.ctx = this.parent.getContext('2d');
 	};
 
 	window.Ad = ad;
