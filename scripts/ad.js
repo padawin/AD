3e8ce94f41e273909a7c9172063bf2d9ee12ff02
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2014-05-23 09:15:19 +0100

    method to generate nb pseudo random colors implemented. the first color is random, the nb-1 next are equaly spreaded from the first

--- scripts/ad.js
+++ scripts/ad.js
@@ -13,15 +13,53 @@
 		this.ctx = this.parent.getContext('2d');
 	};
 
-	var generateColors = function(nb){
-		// generate 1 random color
-		// generate its opposite color
-		// generate nb-2 color equaly dispatched between the boundaries
-		// lerp?
-	}
 
 	var _randomInt = function(maxExcluded) {
 		return 0 | Math.random() * maxExcluded;
+	};
+
+	/**
+	 * Generate an array of colors. A color is an array of 3 integer(0, 255).
+	 * The first color is randomly generated, the last one is the opposite of
+	 * the first one. The others are calculated with a lerp computation.
+	 */
+	var _generateColors = function(nb) {
+		var colors = [],
+			c = 1,
+			last = nb - 1,
+			percentColor,
+			_generateRandomColor,
+			_lerp;
+
+		_generateRandomColor = function() {
+			return [
+				_randomInt(256),
+				_randomInt(256),
+				_randomInt(256)
+			]
+		};
+
+		_lerp = function(val1, val2, ratio) {
+			return 0 | (Math.min(val1, val2) + Math.abs(val1 - val2) * ratio);
+		};
+
+		colors[0] = _generateRandomColor();
+		colors[last] = [
+			(colors[0][0] + 128) % 256,
+			(colors[0][1] + 128) % 256,
+			(colors[0][2] + 128) % 256
+		];
+
+		for (; c < last; c++) {
+			percentColor = c / last;
+			colors[c] = [
+				_lerp(colors[0][0], colors[last][0], percentColor),
+				_lerp(colors[0][2], colors[last][1], percentColor),
+				_lerp(colors[0][1], colors[last][2], percentColor)
+			];
+		}
+
+		return colors;
 	};
 
 	window.Ad = ad;
